8949853f66e83e6efd88b0d309e55e1487bc4620
Stepan <91005125+Stepan-Borchuk@users.noreply.github.com>
2023-08-30 21:02:05 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,61 @@
 import Form from 'Form/Form';
 import { Box } from './Box';
 import React, { Component } from 'react';
-import ContactList from './Contacts/ContactList';
-import { nanoid } from 'nanoid';
-import PropTypes from 'prop-types';
+// import ContactList from './Contacts/ContactList';
+// import { nanoid } from 'nanoid';
+// import PropTypes from 'prop-types';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+ 
   };
 
   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+    // const contacts = localStorage.getItem('contacts');
+    // const parsedContacts = JSON.parse(contacts);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
+    // if (parsedContacts) {
+    //   this.setState({ contacts: parsedContacts });
+    // }
   }
 
   componentDidUpdate(prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
+    // if (this.state.contacts !== prevState.contacts) {
+    //   localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    // }
   }
 
-  submitForm = (values, { resetForm }) => {
-    const oldContact = this.state.contacts.find(
-      person => person.name.toLowerCase() === values.name.toLowerCase()
-    );
+  // submitForm = (values, { resetForm }) => {
+  //   // const oldContact = this.state.contacts.find(
+  //   //   person => person.name.toLowerCase() === values.name.toLowerCase()
+  //   // );
 
-    if (oldContact) {
-      alert(` ${values.name} is already in contacts.`);
-      return;
-    }
+  //   // if (oldContact) {
+  //   //   alert(` ${values.name} is already in contacts.`);
+  //   //   return;
+  //   // }
 
-    const person = {
-      id: nanoid(),
-      name: values.name,
-      number: values.number,
-    };
+  //   // const person = {
+  //   //   id: nanoid(),
+  //   //   name: values.name,
+  //   //   number: values.number,
+  //   // };
 
-    this.setState(prevState => {
-      return {
-        contacts: [person, ...prevState.contacts],
-      };
-    });
-    resetForm();
-  };
+  //   // this.setState(prevState => {
+  //   //   return {
+  //   //     contacts: [person, ...prevState.contacts],
+  //   //   };
+  //   // });
+  //   // resetForm();
+  // };
 
-  onDelete = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(c => c.id !== id),
-      };
-    });
-  };
+  // onDelete = id => {
+  //   // this.setState(prevState => {
+  //   //   return {
+  //   //     contacts: prevState.contacts.filter(c => c.id !== id),
+  //   //   };
+  //   // });
+  // };
 
   render() {
     return (
@@ -79,7 +74,7 @@ class App extends Component {
         borderRadius="normal"
         boxShadow="4px 11px 49px 1px #d7dead"
       >
-        <h1>Phonebook</h1>
+        {/* <h1>Phonebook</h1>
         <Form submitForm={this.submitForm} />
 
         {this.state.contacts.length > 0 && (
@@ -87,7 +82,7 @@ class App extends Component {
             contactsInfo={this.state.contacts}
             deleteContact={this.onDelete}
           />
-        )}
+        )} */}
       </Box>
     );
   }
@@ -95,8 +90,8 @@ class App extends Component {
 
 export default App;
 
-ContactList.propTypes = {
+{/* ContactList.propTypes = {
   contactsInfo: PropTypes.arrayOf(PropTypes.shape),
   deleteContact: PropTypes.func,
   submitForm: PropTypes.func,
-};
+}; */}
